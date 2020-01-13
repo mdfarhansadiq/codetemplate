fe54b8e6faf210949b502492bc26a7a3fe2fc9c5
Md. Farhan Sadiq <42261830+mdfarhansadiq@users.noreply.github.com>
2020-01-13 22:47:44 +0600

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,13 @@
+///******* In the name of Allah *******///
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long int a,b,c,d,e,f,g,t,i,j,k,l,m,n,r,sum,ans_1,ans_2,ans_3,cnt,Max,Min;
+    double aa,bb,cc,dd,ee;
+    char aaa[1013],bbb[1013],ccc[1013];
+
+    return 0;
+}
